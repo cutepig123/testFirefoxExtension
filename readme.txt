557ca19142beffc4698d7a919dc0a61b975424b3
hjs <hjs>
2015-10-04 00:27:15 +0800

    learn how to add to tool menu

---
+++ readme.txt
@@ -0,0 +1,16 @@
+- Example1 xulschoolhello1
+学习
+	编写extension需要的基本文件
+	什么是install.rdf文件
+	什么是chrome.manifest
+	三个必须的子目录是啥？
+	什么是overlay？
+	什么是chrome？
+	什么是xul文件？
+	如何指定xul文件的URI？
+	什么是dtd文件？
+	如何编写dtd中的字符串？
+	如何在xul里面引用dtd里面的字串？
+	如何在菜单栏添加一项子菜单
+	如何设置子菜单的oncommand？
+https://developer.mozilla.org/en-US/Add-ons/Overlay_Extensions/XUL_School/Getting_Started_with_Firefox_Extensions
--- xulschoolhello1/content/browserOverlay.js
+++ xulschoolhello1/content/browserOverlay.js
@@ -13,8 +13,8 @@ XULSchoolChrome.BrowserOverlay = {
    * Says 'Hello' to the user.
    */
   sayHello : function(aEvent) {
-    let stringBundle = document.getElementById("hejinshouhello-string-bundle");
-    let message = stringBundle.getString("hejinshouhello.greeting.label");
+    let stringBundle = document.getElementById("xulschoolhello-string-bundle");
+    let message = stringBundle.getString("xulschoolhello.greeting.label");
 
     window.alert(message);
   }
